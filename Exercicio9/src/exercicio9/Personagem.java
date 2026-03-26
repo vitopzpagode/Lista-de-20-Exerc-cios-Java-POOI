5c88ef2ea1cd025616617c3e545318890f371d40
Vitor <126506463+vitopzpagode@users.noreply.github.com>
2026-03-26 14:30:18 -0300

    Add files via upload

---
+++ Exercicio9/src/exercicio9/Personagem.java
@@ -0,0 +1,42 @@
+
+package exercicio9;
+
+class Personagem {
+    private String nome;
+    private int vida;
+    private int nivel;
+
+    public Personagem(String nome, int vida, int nivel) {
+        this.nome = nome;
+        this.vida = vida;
+        this.nivel = nivel;
+    }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public int getVida() { return vida; }
+    public void setVida(int vida) { this.vida = vida; }
+
+    public int getNivel() { return nivel; }
+    public void setNivel(int nivel) { this.nivel = nivel; }
+
+    public void atacar() {
+        System.out.println("O personagem atacou");
+    }
+
+    public void defender() {
+        System.out.println("O personagem se defendeu");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
